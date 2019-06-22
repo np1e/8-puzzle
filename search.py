9a630fa5d2c4d8be1e926fb1b8778918b5c3baae
Nick Hafkemeyer <nick.hafkemeyer@gmail.com>
2019-06-22 10:05:19 +0200

    improved a* algorithm and added verbose flag to algorithms

--- search.py
+++ search.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 import node
-from queue import Queue, LifoQueue
+from queue import Queue, LifoQueue, PriorityQueue
 from enum import Enum
-
+from itertools import count
 
 class Algorithm(Enum):
     depth_first = 0
@@ -21,25 +21,27 @@ class Search():
             if args:
                 result = self.depthFirst(*args)
             else:
-                result = self.depthFirst()
+                result = self.depthFirst(*args)
         elif algorithm == Algorithm.breadth_first:
-            result = self.breadthFirst()
+            result = self.breadthFirst(*args)
         elif algorithm == Algorithm.iter_deepening:
-            result = self.iterativeDeepening()
+            result = self.iterativeDeepening(*args)
         elif algorithm == Algorithm.a_star:
-            result = self.a_search()
+            result = self.a_search(*args)
 
         endTime = datetime.now()
         diff = endTime-startTime
         print(f"Algorithm took {diff} to return with result {result}")
+        return diff, result
 
-    def breadthFirst(self):
+    def breadthFirst(self, verbose = True):
         queue = Queue()
         queue.put(self.start)
         reached = []
         while queue:
             node = queue.get()
-            #print(f"Current node: {str(node)}")
+            if verbose:
+                print(f"Current node: {str(node)}")
             if node.isGoal():
                 return node
             elif not node.isIn(reached):
@@ -49,23 +51,22 @@ class Search():
                     queue.put(s)
         return None
 
-    def iterativeDeepening(self):
+    def iterativeDeepening(self, verbose = True):
         depth = 1
         result = None
         while result is None:
-            result = self.depthFirst(depth)
+            result = self.depthFirst(depth, verbose)
             depth += 1
         return result
 
-    def depthFirst(self, max_depth = 15):
+    def depthFirst(self, max_depth = 15, verbose = True):
         queue = LifoQueue()
         queue.put(self.start)
         reached = []
-        while queue:
-            if queue.empty():
-                return None
+        while not queue.empty():
             node = queue.get()
-            print(f"Current node: {str(node)}")
+            if verbose:
+                print(f"Current node: {str(node)}")
             if node.isGoal():
                 return node
             elif node.depth != max_depth and not node.isIn(reached):
@@ -77,8 +78,27 @@ class Search():
         return None
 
 
-    def a_search(self):
-        pass
+
+    def a_search(self, heuristic, verbose= True):
+        counter = count()
+        queue = PriorityQueue()
+        queue.put((self.start.heur(heuristic), next(counter), self.start))
+        reached = []
+
+        while not queue.empty():
+            node = queue.get()[2]
+            if verbose:
+                print(f"Current node: {str(node)}")
+            if node.isGoal():
+                return node
+            elif not node.isIn(reached):
+                reached.append(node)
+                succs = node.succ()
+                while not succs.empty():
+                    child = succs.get()
+                    queue.put((child.heur(heuristic) + child.cost(), next(counter), child))
+        return None
+
 
 
 
