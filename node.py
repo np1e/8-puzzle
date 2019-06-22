60afe6e2198b15ff116d7cdd33126264d173e2fb
Nick Hafkemeyer <nick.hafkemeyer@gmail.com>
2019-06-22 10:04:36 +0200

    implement heuristics manhatten distance and n wrong tiles

--- node.py
+++ node.py
@@ -1,5 +1,6 @@
 from queue import Queue
 from copy import deepcopy
+import numpy as np
 
 class Node():
     def __init__(self, puzzle, parent=None, move=""):
@@ -21,6 +22,8 @@ class Node():
     def isGoal(self):
         return self.state.checkPuzzle()
 
+    def cost(self):
+        return self.depth
 
     def succ(self):
         succs = Queue()
@@ -30,6 +33,30 @@ class Node():
             if p.zero is not self.state.zero:
                 succs.put(Node(p, self, m))
         return succs
+
+    def heur(self, heuristic):
+        return self.wrongTiles() if heuristic == 'n' else self.manhattanDistance()
+
+    def manhattanDistance(self):
+        distance = 0
+        for i in range(1, self.state.size**2):
+            curIndex = np.argwhere(self.state.puzzle == i)[0]
+            goalIndex = np.argwhere(self.state.goal == i)[0]
+            dist = sum([np.abs(a-b) for a,b in zip(curIndex, goalIndex)])
+            distance += dist
+
+        return distance
+
+    def wrongTiles(self):
+        n = 0
+        for i in range(0, self.state.size):
+            for j in range(0, self.state.size):
+                if (i,j) != self.state.zero \
+                        and self.state.puzzle[i][j] != self.state.goal[i][j]:
+                    n +=1
+
+        return n
+        n = sum([1 for x in (self.state.puzzle == self.state.goal).flatten() if not x])
 
 
     def __str__(self):
